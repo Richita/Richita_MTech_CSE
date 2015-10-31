746f56b51565d56b942a111d41be08ab211496cc
ninadingole <needna78@gmail.com>
2015-10-31 16:39:25 +0530

    Flow holder Added

--- src/main/java/com/microkernel/core/flow/support/FlowHolderImpl.java
+++ src/main/java/com/microkernel/core/flow/support/FlowHolderImpl.java
@@ -1,23 +1,56 @@
 package com.microkernel.core.flow.support;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.microkernel.core.flow.Flow;
 import com.microkernel.core.flow.FlowHolder;
+import com.microkernel.core.xml.ProcessDefinitionParser;
 
-public class FlowHolderImpl implements FlowHolder {
+public class FlowHolderImpl implements FlowHolder,InitializingBean {
 
+	@Autowired
+	private ProcessDefinitionParser parser;
+	
+	private String processDefinitionLoc;
 	
 	
+	
+	public String getProcessDefinitionLoc() {
+		return processDefinitionLoc;
+	}
+
+	public void setProcessDefinitionLoc(String processDefinitionLoc) {
+		this.processDefinitionLoc = processDefinitionLoc;
+	}
+
+	public ProcessDefinitionParser getParser() {
+		return parser;
+	}
+
+	public void setParser(ProcessDefinitionParser parser) {
+		this.parser = parser;
+	}
+
+
 	private HashMap<String, Flow> flows = new HashMap<String, Flow>();
 	
+	public FlowHolderImpl(){}
+	
 	public FlowHolderImpl(Collection<Flow> flows) {
+		parseFlows(flows);
+	}
+	
+	private void parseFlows(Collection<Flow> flows) {
 		for(Flow flow: flows){
 			this.flows.put(flow.getName(), flow);
 		}
 	}
-	
+
 	public Flow getFlow(String name) {
 		return this.flows.get(name);
 	}
@@ -25,5 +58,18 @@ public class FlowHolderImpl implements FlowHolder {
 
 	public Collection<Flow> getFlows() {
 		return this.flows.values();
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		if(null == parser)
+			throw new IllegalArgumentException("Parser is Not set cannot parse process-definition.");
+		
+		File f = new File(this.getClass().getClassLoader().getResource(processDefinitionLoc).toString());
+		if(f.exists()){
+			FlowHolder holder = parser.parseXML(f);
+			parseFlows(holder.getFlows());
+		}
+		
 	}
 }
--- src/main/java/com/microkernel/core/xml/support/GenericParserContext.java
+++
@@ -1,9 +0,0 @@
-package com.microkernel.core.xml.support;
-
-import com.microkernel.core.xml.ParserContext;
-
-/**
- * Created by NinadIngole on 9/15/2015.
- */
-public class GenericParserContext implements ParserContext {
-}
